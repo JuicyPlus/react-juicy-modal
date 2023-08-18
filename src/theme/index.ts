22c40957b606a6b4aea1b51e9135881dd6154070
Flik Jeong <jsh852@gmail.com>
2023-08-18 19:47:59 +0900

    feat: create theme

---
+++ src/theme/index.ts
@@ -0,0 +1,28 @@
+const theme = {
+  orange: {
+    primary: '#F0B153',
+    secondary: '#5CAD6E',
+    grey: '#475467',
+    light: '#F5F5F5',
+    dark: '#9E692A',
+    white: '#FFFFFF'
+  },
+  kiwi: {
+    primary: '#219E0D',
+    secondary: '#776F51',
+    grey: '#475467',
+    light: '#F9FFF2',
+    dark: '#195F0E',
+    white: '#FFFFFF'
+  },
+  grape: {
+    primary: '#5936BD',
+    secondary: '#FF4BC2',
+    grey: '#475467',
+    light: '#F2F8EB',
+    dark: '#493F65',
+    white: '#FFFFFF'
+  }
+}
+
+export default theme
